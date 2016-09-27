508f4224323ad4f694ed2ffe54c2094d3e44d119
赵宝 <zhaobao@lubanec.com>
2016-09-27 14:23:15 +0800

    完成

--- js/main.js
+++ js/main.js
@@ -1,6 +1,25 @@
 /**
  * Created by Administrator on 2016/9/25.
  */
+/*
+* 1、创建画布
+* 2、绘制gril图片
+* 3、绘制星星图片
+* 4、绘制多个星星
+* 5、实现星星闪烁功能
+* 6、实现星星随机出现
+* 7、添加鼠标控制事件
+* */
+/*
+* 项目用到的知识点
+* 1、canvas getContext()方法，用来设置2D图片
+* 2、requestAnimFrame（） 循环事件，注意与setTimeout()、setInterval()的方法
+* 3、fillStyle（） 设置或返回用于填充绘画的颜色、渐变或模式
+* 4、fillRect（） 绘制矩形
+* 5、drawImage（）绘制图片 context.drawImage(img,sx,sy,swidth,sheight,x,y,width,height)
+* sx截取的x起点  sy截取的y起点 swidth,sheight 截取的宽高  x 图片放在canvas上的x起点，y 图片放在canvas上的y起点
+* width,height 图片放在canvas上的宽高
+* */
 var can; //canvas
 var ctx; //canvas的2D场景
 var w;  //画布的宽
@@ -29,6 +48,11 @@ var lastTime;
 //俩帧刷新的间隔时间
 var deltaTime;
 
+//判断鼠标是否在图片上
+var switchy = false;
+
+//life星星的亮度
+var life=0;
 
 //页面打开是调用的方法
 function init(){
@@ -36,6 +60,9 @@ function init(){
     ctx=can.getContext("2d");
     w=can.width;
     h=can.height;
+
+    //绑定鼠标移入事件
+    document.addEventListener('mousemove', mousemove, false);
 
     girlPic.src="../img/girl.jpg";
     starPic.src="../img/star.png";
@@ -60,6 +87,7 @@ function gameloop(){
     drawBackground();
     drawGril();
     drawStars();
+    aliveUpdate();
 }
 //给画布填充颜色
 function drawBackground(){
@@ -70,4 +98,17 @@ function drawBackground(){
 function drawGril(){
     //drawImag(img,x,y)
     ctx.drawImage(girlPic,padLeft,padTop,girlWidth,girlHeight)
+}
+
+function mousemove(e){
+    if(e.offsetX|| e.layerX){
+        var px = e.offsetX == undefined ? e.layerX : e.offsetX;
+        var py = e.offsetY == undefined ? e.layerY : e.offsetY;
+
+        if (px > padLeft && px < (padLeft + girlWidth) && py > padTop && py < (padTop + girlHeight)) {
+            switchy = true;
+        } else {
+            switchy = false;
+        }
+    }
 }
--- js/stars.js
+++ js/stars.js
@@ -1,12 +1,16 @@
 /**
  * Created by Administrator on 2016/9/26.
  */
+ //绘制星星JS
 //定义一个方法
 var starObj=function(){
     this.x;
     this.y;
     this.picNo;
     this.time;
+
+    this.xSbd; //X轴速度
+    this.ySbd; //Y轴速度
 }
 //初始化，获得坐标值
 starObj.prototype.init=function(){
@@ -14,22 +18,48 @@ starObj.prototype.init=function(){
     this.y=Math.random()*300+150;
     this.picNo=Math.floor(Math.random()*7);
     this.time=0;
+
+    this.xSbd=Math.random()*3-1.5;
+    this.ySpd=Math.random()*3-1.5;
 }
 starObj.prototype.update=function(){
+    this.x+=this.xSbd*deltaTime*0.004;
+    this.y+=this.ySpd*deltaTime*0.004;
+
+    //星星重生
+    if (this.x > (padLeft + girlWidth) || this.x < (padLeft - 10)){
+        this.init();
+        return
+    }
+    if (this.y > (padTop + girlHeight) || this.y < (padTop - 10)){
+        this.init();
+        return
+    }
+
+
+
     this.time+=deltaTime;
     if(this.time>50){
         this.picNo+=1;
-        this.picNo%=7; //这个看不懂
+        //if(this.picNo>=7){
+        //        this.picNo=0;
+        // }
+        this.picNo%=7; //余数为7，意思是picNo必须小于7
         this.time=0;
     }
-
-    //if(this.picNo>=7){
-    //    this.picNo=0;
-    //}
 }
+
 starObj.prototype.draw=function(){
+    //在save和restore中间的操作受影响
+    ctx.save();
+
+    //globalAlpha 全局透明度
+     ctx.globalAlpha=life; //[0,1]
+
     //drawImage(img,sx,sy,swidth.sheight,x,y,width,height)
     ctx.drawImage(starPic,this.picNo*7,0,7,7,this.x,this.y,7,7)
+
+    ctx.restore()
 }
 ////绘制星星
 //function drawStar(){
@@ -41,5 +71,19 @@ function drawStars(){
     for(var i=0;i<num;i++){
         stars[i].draw();
         stars[i].update()
+    }
+}
+
+function aliveUpdate() {
+    if (switchy) {
+        life += 0.03;
+        if (life > 0.7) {
+            life = 0.7;
+        }
+    } else {
+        life -= 0.03;
+        if (life < 0) {
+            life = 0;
+        }
     }
 }
