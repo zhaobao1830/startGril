529f7c251365c2f9c3ef23a58e0425dc5ced86a7
赵宝 <zhaobao@lubanec.com>
2016-12-05 15:58:47 +0800

    公共js

--- js/commonFunctions.js
+++ js/commonFunctions.js
@@ -136,5 +136,28 @@ var eventUtil={
 		}else{
 			element['on'+type]=handler;
 		}
+	},
+	getEvent:function(event){
+		return event?event:window.event;
+	},
+	getType:function(event){
+		return event.type;
+	},
+	getElement:function(event){
+		return event.target || event.srcElement;
+	},
+	preventDefault:function(event){
+		if(event.preventDefault){
+			event.preventDefault();
+		}else{
+			event.returnValue=false;
+		}
+	},
+	stopPropagation:function(event){
+		if(event.stopPropagation){
+			event.stopPropagation();
+		}else{
+			event.cancelBubble=true;
+		}
 	}
 }
